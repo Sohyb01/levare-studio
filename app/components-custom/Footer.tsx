a753ca0e89310972f2ac40f029268aafc190ab33
Sohyb01 <sohyb0155@gmail.com>
2024-06-06 10:13:44 +0200

    footer links update

--- app/components-custom/Footer.tsx
+++ app/components-custom/Footer.tsx
@@ -19,9 +19,14 @@ const Footer = () => {
           </Link>
           {/* Links */}
           <div className="grid grid-cols-1 gap-x-16 gap-y-6 text-detail">
-            <Link href="/home">Home</Link>
+            <Link href="/case-studies/pes">Case Study</Link>
             <Link href="/contact">Work with us</Link>
-            <Link href="#">Gumroad</Link>
+            <Link
+              href="https://www.instagram.com/sonny.branding/"
+              target="_blank"
+            >
+              Instagram
+            </Link>
             {/* <Link href="/locations">Locations</Link>
             <Link href="/team">Our Team</Link>
             <Link href="/gallery">Gallery</Link> */}
