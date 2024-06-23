f706f58254ec86a8bac6edf5dd857fb72122692b
Sohyb01 <sohyb0155@gmail.com>
2024-06-23 23:17:40 +0200

    commented out packages section

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
       <ValueSection />
       <HowItWorksSection />
       <ClientShowcaseSection />
-      <PackagesSection />
+      {/* <PackagesSection /> */}
       <IsThisForMeSection />
       <AboutSection />
       <FAQSection />
